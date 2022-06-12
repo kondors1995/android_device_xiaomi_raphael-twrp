e8dba6e25e3d6b45f37d1064e67cce8aa8a3888e
Pranav Talmale <pranavtalmale@gmail.com>
2022-06-12 13:18:11 +0530

    Add Haptics support

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_APEX := true
 
 # Vibrator
-# disable vibration for testing purposes
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
